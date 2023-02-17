67b4ca3309de796752d0361ecf55a49f8d0c1e4e
nittinsinghh <125678418+nittinsinghh@users.noreply.github.com>
2023-02-17 21:57:56 +0530

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,9 @@
+<?php
+require "lab-panel/include/inc_fe.php";
+$errorMsg = "";
+
+$smarty->assign("errorMsg", $errorMsg);
+$smarty->assign("page_title", "MSDP LAB Tool Availability");
+
+$smarty->display("welcome.tpl");
+?>
